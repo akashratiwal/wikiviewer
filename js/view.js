1f312e874e9139148a03368c65b1677106aa6853
akash_verma_ <akash.verma@okutech.in>
2016-01-26 19:35:16 +0530

    Just Search Bar created

---
+++ js/view.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#search").click(function(){
+      $("#search").css("display", "none");
+      $("#inside_container").css("display", "inline");
+      $("#inside_container").animate({"width" : "300px"});
+      $("#search").css("width","0");
+  });
+  $("#close_button").click(function() {
+    $("#text_bar").val("");
+    $("#inside_container").animate({"width" : "0px"});
+    $("#inside_container").css("display", "none");
+    $("#search").css("display","inline");
+    $("#search").animate({"width":"60px"});
+  });
+  $("#text_bar").keypress(function(e) {
+    if (e.which == 13) {
+      
+    }
+  });
+});
