8bff7530c6ab635d87686e38b6440782169898ac
akash_verma_ <akash.verma@okutech.in>
2016-01-28 00:57:43 +0530

    Base Design Completed

--- js/view.js
+++ js/view.js
@@ -11,10 +11,25 @@ $(document).ready(function() {
     $("#inside_container").css("display", "none");
     $("#search").css("display","inline");
     $("#search").animate({"width":"60px"});
+    $("#result_container").hide({"duration": "300"});
+    $("#container").animate({"margin-top":"150px"});
   });
   $("#text_bar").keypress(function(e) {
     if (e.which == 13) {
-      $("#container").css("margin-top", "20px");
+      $("#container").animate({"margin-top":"20px"});
+      $("#result_container").hide({"duration": "300"});
+      $.ajax({
+        url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + $("#text_bar").val(),
+        dataType: 'jsonp',
+        success: function (val) {
+          $("#result_container").html("");
+          for (var i in val.query.pages) {
+            $("#result_container").append("<a href='" + "https://en.wikipedia.org/?curid=" + val.query.pages[i].pageid + "' target='_blank'><div class='result_items'><span class='title'>" + val.query.pages[i].title +
+            "</span><br><br><span class='extract'>" + val.query.pages[i].extract + "</span></div></a>");
+          }
+          $("#result_container").show({"duration": "300"});
+      }
+      });
     }
   });
 });
